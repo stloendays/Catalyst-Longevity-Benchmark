0afa7c699e1449848ebf11b50dc76ebab0ec9857
Tony <157110190+stloendays@users.noreply.github.com>
2026-09-11 21:05:52 +0800

    Declare Tony embedded interaction frames

---
+++ bear_agent_pet/client/src/EmbeddedActionAssetsInternal.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <QPixmap>
+#include <QVector>
+namespace TonyEmbeddedActionsInternal {
+QVector<QPixmap> loadPat();
+QVector<QPixmap> loadLifted();
+QVector<QPixmap> loadLanding();
+QVector<QPixmap> loadDizzy();
+QVector<QPixmap> loadStretch();
+QVector<QPixmap> loadYawn();
+QVector<QPixmap> loadCursorWatch();
+}
