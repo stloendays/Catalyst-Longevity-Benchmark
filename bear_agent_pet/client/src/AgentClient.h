848318bfbc2710ad2164500b76786ac1ecebab3a
Tony <157110190+stloendays@users.noreply.github.com>
2026-09-11 18:11:23 +0800

    Add Tony local tool protocol to AgentClient

--- bear_agent_pet/client/src/AgentClient.h
+++ bear_agent_pet/client/src/AgentClient.h
@@ -4,6 +4,7 @@
 #include <QUrl>
 #include <QTimer>
 #include <QNetworkAccessManager>
+#include <QJsonObject>
 
 class AgentClient : public QObject {
     Q_OBJECT
@@ -12,6 +13,11 @@ public:
     void connectTo(const QUrl &url, const QString &bearerToken={});
     void pairAndConnect(const QUrl &wsUrl, const QString &pairingCode, const QString &deviceName);
     void sendMessage(const QString &text);
+    void sendToolResult(const QString &requestId,
+                        const QString &tool,
+                        bool ok,
+                        const QJsonObject &result={},
+                        const QString &error={});
     bool connected() const;
     QString bearerToken() const { return bearerToken_; }
 signals:
@@ -22,12 +28,14 @@ signals:
     void connectionChanged(bool connected);
     void paired(const QString &token, const QString &deviceId, const QUrl &endpoint);
     void pairingFailed(const QString &text);
+    void toolRequest(const QString &requestId, const QString &tool, const QJsonObject &args);
     void errorMessage(const QString &text);
 private slots:
     void onText(const QString &message);
     void reconnect();
 private:
     static QUrl pairingUrlFor(const QUrl &wsUrl);
+    void sendClientHello();
 
     QWebSocket socket_;
     QNetworkAccessManager network_;
