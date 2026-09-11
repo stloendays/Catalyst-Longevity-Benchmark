ecb9ea39d0960527b2ec2d03fdfe508d4a80ae37
Tony <157110190+stloendays@users.noreply.github.com>
2026-09-11 21:25:56 +0800

    Enable Tony automatic local update checks

--- teddy_agent_pet/client/src/main.cpp
+++ teddy_agent_pet/client/src/main.cpp
@@ -1,13 +1,19 @@
 #include <QApplication>
 #include <QCoreApplication>
 #include "PetWindow.h"
+#include "UpdateManager.h"
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     QCoreApplication::setOrganizationName("TonyAgent");
     QCoreApplication::setApplicationName("Tony Desktop Pet");
+    QCoreApplication::setApplicationVersion("0.8.4");
     app.setQuitOnLastWindowClosed(false);
+
     PetWindow pet;
+    UpdateManager updater(&pet);
     pet.show();
+    updater.scheduleAutomaticCheck();
+
     return app.exec();
 }
