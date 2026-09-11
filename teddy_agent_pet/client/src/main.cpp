5d5721db76e8402576712759a27dea48428b9273
Tony <157110190+stloendays@users.noreply.github.com>
2026-09-11 22:12:16 +0800

    Restore legacy branch application metadata

--- teddy_agent_pet/client/src/main.cpp
+++ teddy_agent_pet/client/src/main.cpp
@@ -1,18 +1,13 @@
 #include <QApplication>
 #include <QCoreApplication>
-#include <QString>
 #include "PetWindow.h"
 #include "UpdateManager.h"
-
-#ifndef TONY_APP_VERSION
-#error "TONY_APP_VERSION must be provided by CMake"
-#endif
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     QCoreApplication::setOrganizationName("TonyAgent");
     QCoreApplication::setApplicationName("Tony Desktop Pet");
-    QCoreApplication::setApplicationVersion(QString::fromLatin1(TONY_APP_VERSION));
+    QCoreApplication::setApplicationVersion("0.8.4");
     app.setQuitOnLastWindowClosed(false);
 
     PetWindow pet;
