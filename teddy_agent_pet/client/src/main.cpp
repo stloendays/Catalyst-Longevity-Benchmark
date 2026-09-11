c11f9b8afad1c1c75b651ce81c6c8bd15ebb97ce
Tony <157110190+stloendays@users.noreply.github.com>
2026-09-11 22:03:36 +0800

    Use centralized Tony version in application metadata

--- teddy_agent_pet/client/src/main.cpp
+++ teddy_agent_pet/client/src/main.cpp
@@ -1,13 +1,18 @@
 #include <QApplication>
 #include <QCoreApplication>
+#include <QString>
 #include "PetWindow.h"
 #include "UpdateManager.h"
+
+#ifndef TONY_APP_VERSION
+#error "TONY_APP_VERSION must be provided by CMake"
+#endif
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     QCoreApplication::setOrganizationName("TonyAgent");
     QCoreApplication::setApplicationName("Tony Desktop Pet");
-    QCoreApplication::setApplicationVersion("0.8.4");
+    QCoreApplication::setApplicationVersion(QString::fromLatin1(TONY_APP_VERSION));
     app.setQuitOnLastWindowClosed(false);
 
     PetWindow pet;
