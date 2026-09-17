adf8277b9b6416ae208e80f8012fba29ab4340bd
Tony <157110190+stloendays@users.noreply.github.com>
2026-09-17 18:45:50 +0800

    feat: add persistent local reminder manager

---
+++ bear_agent_pet/client/src/LocalReminderManager.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <QDateTime>
+#include <QJsonArray>
+#include <QObject>
+#include <QTimer>
+
+class LocalReminderManager : public QObject {
+    Q_OBJECT
+public:
+    explicit LocalReminderManager(QObject *parent=nullptr);
+
+    QString createReminder(const QString &title, const QString &text, const QDateTime &dueUtc);
+    QJsonArray reminders() const;
+    bool cancelReminder(const QString &id);
+    int count() const;
+
+signals:
+    void reminderDue(const QString &id, const QString &title, const QString &text);
+
+private:
+    struct Reminder {
+        QString id;
+        QString title;
+        QString text;
+        QDateTime dueUtc;
+    };
+
+    void load();
+    void save() const;
+    void scheduleNext();
+    void processDue();
+
+    QList<Reminder> items_;
+    QTimer timer_;
+};
